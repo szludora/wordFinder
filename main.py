bb716a9d8b3c34e16bd48650e926dc096b5d06db
Daergaoth <67022453+Daergaoth@users.noreply.github.com>
2026-04-01 20:29:55 +0200

    #7 Randomize table and alternative search algorythm (#15)
    * #7 Randomize table and alternative search algorythm
    
    * #7 Make dynamic table optional
    
    * #7 Define correct search direction
    
    * #7 Fix typo
    
    * #7 KMP explanation in code
    
    * #7 Merge finder.py
    
    * #7 add config.json
    
    * #7 better names and comments for dynamic table build
    
    * #7 better explanation for KMP algorythm
    
    * #7 Cleaner parameter import process
    
    * #7 Fix typos, add tests for KMP and dynamic table generation. 96% test coverage
    
    * #7 Fix enum value change impact on JSON import
    
    * #7 Fix enum value change impact on JSON import
    
    * #7 Improve test
    
    * #7 Fix typo
    
    * #7 improve kmp algorythm with index limit check and palindrome check
    
    * #7 Add tests and fix failing test on pipeline
    
    * #7 Add missing assert

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import sys
+import time
 import io
-import data.datas as tables
-from data.direction import Direction
+
+from data.parameters import Parameters
 from scripts.finder import find_words_in_table
+from scripts.build_dynamic_data import build_dynamic_data
+from scripts.import_params import import_params
 from scripts.print_table import print_formatted_table
 
-_DIRECTIONS_TO_SEARCH = [
-    Direction.HORIZONTAL,
-    Direction.VERTICAL,
-    Direction.DIAGONAL,
-]
-
-
-def main(table=tables.TABLE_1, directions_to_search=_DIRECTIONS_TO_SEARCH) -> None:
+def main(parameters: Parameters) -> None:
     if "pytest" not in sys.modules:
         if isinstance(sys.stdout, io.TextIOWrapper):
             sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
         if isinstance(sys.stderr, io.TextIOWrapper):
             sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8")
+            
+    if parameters.build_dynamic_table:
+        build_dynamic_data(parameters)
 
-    print_formatted_table(table["table"])
-
-    find_words_in_table(
-        table=table["table"],
-        words=table["words"],
-        directions=directions_to_search,
-    )
+    print_formatted_table(parameters.table)
+    start = time.time()
+    find_words_in_table(parameters)
+    print(f"Elapsed time: {(time.time() - start):.2f} seconds")
 
 
 if __name__ == "__main__":
-    main()
+    use_custom_params: bool = '--params' in sys.argv
+    params: Parameters = import_params(use_custom_params)
+    main(params)
